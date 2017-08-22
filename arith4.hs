f14837faa929d9683b0c81f65d2c3116aff14883
E-Lynn Yap <yapelynn@gmail.com>
2017-08-22 11:58:25 -0400

    Exercises 4-6 of ch 7

---
+++ arith4.hs
@@ -0,0 +1,16 @@
+module Arith4 where
+
+-- id :: a -> a
+-- id x = x
+
+roundTrip :: (Show a, Read a) => a -> a
+roundTrip a = read (show a)
+
+-- Pointfree version
+roundTrip2 :: (Show a, Read b) => a -> b
+roundTrip2 a = read . show $ a
+
+main = do
+  print (roundTrip 4)
+  print (id 4)
+  print (roundTrip2 4 :: Int)
